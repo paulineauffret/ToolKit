7e2835f352291ea788952251de4099b1f5957fb6
paulineauffret <paulineauffret@hotmail.com>
2018-08-14 09:33:39 -1000

    clean up

--- get_fasta.py
+++ get_fasta.py
@@ -1,13 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#Import libraries
 import sys
 
-fileIN=open(sys.argv[1], "rU") #transcriptome
-fileID=open(sys.argv[2], "rU")	#IDs list to get fasta sequence
-fileOUT=open(sys.argv[3], "w")	#out file
+###################################################################
+#Set input files
+fasta=sys.argv[1] 			#transcriptome or genome fasta file
+idlist=sys.argv[2]			#IDs list to get fasta sequence (text file; one id per line, no header)
+output=sys.argv[3]			#output file
 
+#Open files
+try:
+	fileIN=open(fasta, "rU")
+except IOError, e:      		
+	print "Error : cannot open file ", fasta
+
+try:
+	fileID=open(idlist, "rU")
+except IOError, e:      		
+	print "Error : cannot open file ", idlist
+
+try:
+	fileOUT=open(output, "w")
+except IOError, e:      		
+	print "Error : cannot open file ", output
+
+###################################################################
+#Initialize dictionary
 ref=dict()
 
-
-
+#Read fasta file
 line=fileIN.readline()
 seq=""
 id=""
@@ -23,21 +46,27 @@ while line :
 		seq=seq+line
 	line=fileIN.readline()
 ref[str(id)]=str(seq)
+print("There are "+str(nbSeq)+" sequences in input transcriptome/genome.")
 
-print("Il y a "+str(nbSeq)+" sequences dans le transcriptome.")
 #print(ref)
-print(len(ref.keys()))
+#print(len(ref.keys()))
 
-
+###################################################################
+#Read id file
 dup=dict()
 line=fileID.readline()
+nb_id=0
 while(line) :
 	line=line.replace("\n","").replace(">","")
 	if str(line) != "" :
+		nb_id=nb_id+1
 		if not ref.has_key(line) :
-			print("Erreur : sequence "+str(line)+" non presente dans le transcriptome.")	
+			print("Error : sequence "+str(line)+" not present in input transcriptome/genome.")	
 		elif not dup.has_key(line) :
 			fileOUT.write(">"+line+"\n")
 			fileOUT.write(ref[line])
 	dup[str(line)]=1
 	line=fileID.readline()
+print(str(nb_id)+" sequences printed in "+str(output)+" file.")
+
+
